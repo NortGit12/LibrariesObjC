4f994b530a438e3cae1ffc5339167dd997463928
Jeff Norton <jeff12ntn@gmail.com>
2016-11-29 14:28:34 -0700

    Initial commit of empty project

---
+++ LibrariesObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LibrariesObjC
+//
+//  Created by Jeff Norton on 11/29/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LibrariesObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LibrariesObjC
+//
+//  Created by Jeff Norton on 11/29/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
